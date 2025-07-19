6e5353cb00892b4a84a3802fb0b93b201f6a3533
Felixt <felix.tessier9@gmail.com>
2025-07-19 08:36:04 -0500

    refactor: remove sample articles from CardData and update resume

--- src/data/ArticlesCardData.tsx
+++ src/data/ArticlesCardData.tsx
@@ -16,28 +16,4 @@ export const CardData: CardDataType[] = [
     authorAvatar: '/images/felix-profile.jpg',
     readTime: '6 min read',
   },
-  {
-    title: 'Understanding React Hooks',
-    slug: 'understanding-react-hooks',
-    coverSrc: '/images/software-dev.jpeg',
-    authorName: 'Felix',
-    authorAvatar: '/images/felix-profile.jpg',
-    readTime: '8 min read',
-  },
-  {
-    title: 'CSS Grid vs Flexbox',
-    slug: 'css-grid-vs-flexbox',
-    coverSrc: '/images/software-dev.jpeg',
-    authorName: 'Felix',
-    authorAvatar: '/images/felix-profile.jpg',
-    readTime: '5 min read',
-  },
-  {
-    title: 'The Future of JavaScript',
-    slug: 'future-of-javascript',
-    coverSrc: '/images/software-dev.jpeg',
-    authorName: 'Felix',
-    authorAvatar: '/images/felix-profile.jpg',
-    readTime: '7 min read',
-  },
 ];
